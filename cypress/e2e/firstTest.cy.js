de2e48a5855a47c29f85c072b9aab5eedd57aac8
Margarita <margarita.atmb@gmail.com>
2022-06-25 23:26:14 +1000

    Поиск элементов: Contains

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,12 +1,21 @@
 /// <reference types="Cypress" />
-// it('By Contains name', () => {
-//     cy.visit('https://htmlacademy.ru/study')
-//     cy.get('[class^="page-content"]')
-// });
 
-it('Using Get with Find and Eq', () => {
-    cy.visit('https://htmlacademy.ru/study')
-    cy.get('.page-header__top').find('a').eq(10)
-});
+it('', () => {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.contains('Войти'); //поиск элемента по значению
+})
 
-//comment
+it('', () => {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.contains('a','Войти'); //поиск игнорируя приоритетные input, button, a, label 
+})
+
+it('', () => {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.contains('ВОЙТИ', {matchCase: false}); //игнорируем регистр
+})
+
+it.only('', () => {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.get('header').contains('ВОЙТИ', {matchCase: false}); //сочетание с get
+})
