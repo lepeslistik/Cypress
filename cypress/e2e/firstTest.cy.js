46e7e0c283bdac832553326cfb88fc52770a3b93
Margarita <margarita.atmb@gmail.com>
2022-06-27 21:30:09 +1000

    Проверки Should and Expect

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,12 +1,27 @@
 /// <reference types="Cypress" />
-// it('By Contains name', () => {
-//     cy.visit('https://htmlacademy.ru/study')
-//     cy.get('[class^="page-content"]')
-// });
 
-it('Using Get with Find and Eq', () => {
-    cy.visit('https://htmlacademy.ru/study')
-    cy.get('.page-header__top').find('a').eq(10)
-});
+it.only('SHOULD', ()=> {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.get('[id="login-password"]')
+    .type(100) //меняем значение
+    .should('have.value', 100) //проверяем изменилось ли значение
+    .and('be.visible') //проверяем, что поле видимо
+})
 
-//comment
+it('SHOULD', ()=> {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.get('[id="login-password"]')
+    .type(100).then( input => {
+        expect(input).to.have.value(100)
+    })
+})
+
+it('SHOULD', ()=> {
+    cy.visit('https://htmlacademy.ru/login?redirect_url=https%3A%2F%2Fhtmlacademy.ru%2Fstudy')
+    cy.get('.main-nav__dropdown-inner>ul>li').eq(1)
+    //cy.contains('Все курсы')
+        .trigger('mouseover')
+       // .contains('')
+       // .should('be.visible')
+    
+})
