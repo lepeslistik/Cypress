1e5c2c96bcad01dc5a7eb6504a96a87ac60005b2
Margarita <margarita.atmb@gmail.com>
2022-07-05 01:02:37 +1000

    API-HTTP requests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:1234",
+    // baseUrl: "http://localhost:1234",
     excludeSpecPattern: "cypress/e2e/{1*,2*}/*",
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,12 +1,52 @@
 /// <reference types="Cypress" />
-// it('By Contains name', () => {
-//     cy.visit('https://htmlacademy.ru/study')
-//     cy.get('[class^="page-content"]')
-// });
 
-it('Using Get with Find and Eq', () => {
-    cy.visit('https://htmlacademy.ru/study')
-    cy.get('.page-header__top').find('a').eq(10)
-});
+it('Get', () => {
+    cy.request('https://stage.dobrovmeste.com/')
+        .then((response)=>{
+            console.log(response);
+        })  
+})
 
-//comment
+it('Post', () => {
+    cy.request({
+        method: 'POST',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        body: {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+          },
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          }
+    })
+        .then((response)=>{
+            expect(response).to.have.property('status').to.equal(201)
+            expect(response.body).to.have.property('id').to.equal(101)
+            console.log(response);
+        })  
+})
+
+it('Post', () => {
+    cy.request({
+        method: 'POST',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        body: {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+          },
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          }
+    })
+        .its('body').should('contain', {
+            id: 101
+        })  
+})
+
+it.only('', () => {
+    cy.visit('https://stage.dobrovmeste.com/sign_in')
+        //.get('[role="submit"]')
+        .contains('[role="submit"]', 'Войти')
+})
