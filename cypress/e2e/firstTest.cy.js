b7e765416173d904fcc8de201c6e601d2785a0e6
Margarita <margarita.atmb@gmail.com>
2022-07-05 01:12:56 +1000

    Mock, Stub

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,12 +1,5 @@
 /// <reference types="Cypress" />
-// it('By Contains name', () => {
-//     cy.visit('https://htmlacademy.ru/study')
-//     cy.get('[class^="page-content"]')
-// });
 
-it('Using Get with Find and Eq', () => {
-    cy.visit('https://htmlacademy.ru/study')
-    cy.get('.page-header__top').find('a').eq(10)
-});
-
-//comment
+it('', () => {
+    
+})
